da35d925d1c3cc9c944bfdf0461f5410c45513b6
Moritz <moritz.boehm@campus.lmu.de>
2024-07-02 20:48:38 +0200

    add async to generator plugin

--- src/js/instrument/esnstrument.js
+++ src/js/instrument/esnstrument.js
@@ -39,7 +39,7 @@ if (typeof J$ === 'undefined') {
 		    var res = babel.transform(code, {
 		      retainLines: true,
 		      presets: ['es2017','es2015'],
-              plugins: ['transform-runtime'],
+              plugins: ['transform-runtime', 'transform-async-to-generator'],
 		    }).code; 
 
 		    if (res && res.indexOf('use strict') != -1) {
